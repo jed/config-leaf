db79a1f1ba345c20f581775d1e134e16bf38a332
ahadik <ahadik@us.ibm.com>
2016-05-21 14:06:34 -0500

    Pass an environmental variable name as a command line argument with the --PW flag to encrypt and decrypt using the value of the supplied environmental variable key.

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-var crypto   = require("crypto")
-var fs       = require("fs")
+var crypto	 = require("crypto")
+var fs			 = require("fs")
 var readline = require("readline")
-var path     = require("path")
+var path		 = require("path")
 
 module.exports = function(fn) {
-  var from = path.join(process.cwd(), process.argv[2])
-  var to   = path.join(process.cwd(), process.argv[3])
+	var argv = require('minimist')(process.argv.slice(2));
+	var from = path.join(process.cwd(), process.argv[2])
+	var to	 = path.join(process.cwd(), process.argv[3])
 
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
+	var rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	})
 
-  var input = function(query, callback) {
-    var stdin = process.openStdin(),
-        i = 0;
-    process.stdin.on("data", function(char) {
-        char = char + "";
-        switch (char) {
-            case "\n":
-            case "\r":
-            case "\u0004":
-                stdin.pause();
-                break;
-            default:
-                process.stdout.write("\033[2K\033[200D" + Array(rl.line.length+1).join("*"));
-		i++;
-                break;
-        }
-    });
+	var input = function(query, callback) {
+		if (argv.PW){
+      if(process.env[argv.PW]){
+        callback(process.env[argv.PW]);
+        process.exit();
+      }else{
+        console.error(new Error("Environmental variable \""+argv.PW+"\" has not been set."));
+        process.exit(1)
+      }
+      
+		}else{
+			var stdin = process.openStdin(),
+					i = 0;
+			process.stdin.on("data", function(char) {
+					char = char + "";
+					switch (char) {
+							case "\n":
+							case "\r":
+							case "\u0004":
+									stdin.pause();
+									break;
+							default:
+									process.stdout.write("\033[2K\033[200D" + Array(rl.line.length+1).join("*"));
+			i++;
+									break;
+					}
+			});
 
-    rl.question(query, function(value) {
-        rl.history = rl.history.slice(1);
-        callback(value);
-    });
-  }
-  
-  input("Enter the config password ("+path.basename(to)+"):\n", function(password) {
+			rl.question(query, function(value) {
+					rl.history = rl.history.slice(1);
+					callback(value);
+			});
+		}
+	}
+	
+	input("Enter the config password ("+path.basename(to)+"):\n", function(password) {
     from = fs.createReadStream(from)
     to   = fs.createWriteStream(to)
-    fn   = fn("cast5-cbc", password)
+		fn	 = fn("cast5-cbc", password)
 
-    from.pipe(fn).pipe(to)
-    from.on("end", function () {
-      rl.write("done\n");
-      rl.close.bind(rl);
-    });
-    
-  })
+		from.pipe(fn).pipe(to)
+		from.on("end", function () {
+			rl.write("done\n");
+			rl.close.bind(rl);
+		});
+		
+	})
 }
